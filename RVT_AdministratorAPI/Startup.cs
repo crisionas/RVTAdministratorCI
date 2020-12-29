ad410d7a10eb4f5eea5e3f951e865300da4b9d1f
mrelectrq <tiora100bass@gmail.com>
2020-12-29 20:29:02 +0200

    RabbitMQ configuration definition

--- RVT_AdministratorAPI/Startup.cs
+++ RVT_AdministratorAPI/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RVT_AdministratorAPI.AppServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,8 +27,21 @@ namespace RVT_AdministratorAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(opt =>
+            {
+                var factory = new ConnectionFactory()
+                {
+                    HostName = Configuration["QueueHost"],
+                    UserName = Configuration["RabbitMQUsername"],
+                    Password = Configuration["RabbitMQPassword"],
+                    Port = 5672
+                };
+                return new RabbitMQQueueConnection(factory);
+            });
             services.AddControllers();
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -46,6 +61,33 @@ namespace RVT_AdministratorAPI
             {
                 endpoints.MapControllers();
             });
+            app.UseRabbitListener();
+        }
+    }
+
+    public static class ApplicationBuilderExtensions
+    {
+        public static RabbitMQQueueConnection Listener { get; set; }
+
+        public static IApplicationBuilder UseRabbitListener(this IApplicationBuilder application)
+        
+       {
+            Listener = application.ApplicationServices.GetService<RabbitMQQueueConnection>();
+
+            var lifetime = application.ApplicationServices.GetService<IHostApplicationLifetime>();
+            lifetime.ApplicationStarted.Register(OnStarted);
+            lifetime.ApplicationStopping.Register(OnStoped);
+            return application;
+        }
+
+        private static void OnStoped()
+        {
+            Listener.Disconnect();
+        }
+
+        private static void OnStarted()
+        {
+            Listener.InitReceiverChannel();
         }
     }
 }
