21f1b79f7ac574f9734aab6265475986fa83549e
mrelectrq <tiora100bass@gmail.com>
2020-12-29 20:27:59 +0200

    Edited Models for implementation

---
+++ RVTLibrary/Models/Vote/NodeVoteResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RVTLibrary.Models.Vote
+{
+    public class NodeVoteResponse
+    {
+        public bool Status { get; set; }
+        public string Message { get; set; }
+        public DateTime ProcessedTime { get; set; }
+        public Block block { get; set; }
+        public string IDVN { get; set; }
+    }
+}
---
+++ RVTLibrary/Models/Vote/VoteCoreResponse.cs
@@ -0,0 +1,14 @@
+﻿using RVTLibrary.Models.LoadBalancer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RVTLibrary.Models.Vote
+{
+    public class VoteCoreResponse
+    {
+        public VoteResponse VoteResponse { get; set; }
+        public ChooserLbMessage LBMessage { get; set; }
+
+    }
+}
